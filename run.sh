8b4311d4ea379c1ffd62f64017b8225d67997971
Fir3eye <93431222+Fir3eye@users.noreply.github.com>
2024-01-17 23:27:06 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+# Run Magento setup command
+php -dmemory_limit=-1 bin/magento setup:install \
+    --base-url=http://${SERVER_IP}:8080/ \
+    --db-host=db \
+    --db-name=magentodb \
+    --db-user=magentouser \
+    --db-password=MyPassword \
+    --admin-firstname=Admin \
+    --admin-lastname=User \
+    --admin-email=admin@your-domain.com \
+    --admin-user=admin \
+    --admin-password=admin123 \
+    --language=en_US \
+    --currency=USD \
+    --timezone=America/Chicago \
+    --use-rewrites=1 \
+    --search-engine=elasticsearch7 \
+    --elasticsearch-host=elasticsearch \
+    --elasticsearch-port=9200 \
+    --elasticsearch-index-prefix=magento2 \
+    --elasticsearch-enable-auth=0 \
+    --elasticsearch-timeout=15
+
+# Start Apache (or your web server) in the foreground
+exec apache2-foreground
